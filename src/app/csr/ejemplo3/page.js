30039fc0f529d8d3e80dea3bc9314ee6780c0182
Daniel E. Serrano Portillo <danieu275@gmail.com>
2024-11-15 19:31:44 +0100

    ejemplo 3 js hecho

---
+++ src/app/csr/ejemplo3/page.js
@@ -0,0 +1,42 @@
+'use client'
+import { useState, useEffect } from "react";
+
+
+function Page() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        // fetch('https://dummyjson.com/products')
+        //     .then(res => res.json())
+        //     .then(data => setProducts(data.products));
+
+        const fetchData = async () => {
+            const res = await fetch('https://rickandmortyapi.com/api/character')
+            const data = await res.json()
+
+            setCharacters(data.results)
+        }
+        fetchData()
+
+    }, [])
+
+
+    return (
+        <>      
+             <h2 className="text-xl font-bold text-center">Ejemplo 2: CSR React</h2>
+            <ul>
+                {
+                   characters.map(character => (
+                        <li key={character.id} className="py-20 text-center">
+                            <img src={character.image} alt="foto" className="mx-auto" />
+                            <b>{character.name}:</b> {character.created}
+                        </li>
+                    ))
+
+                }
+            </ul>
+        </>
+    )
+}
+
+export default Page  
